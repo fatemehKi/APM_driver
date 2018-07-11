668e2a73fe71ff97745cfc19a1bb4d70c3cdc872
fatemehKi <40069260+fatemehKi@users.noreply.github.com>
2018-07-11 21:21:09 +0000

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -136,7 +136,7 @@ int main()
         APM_OutputReg[0] = 0x01;
         APM_OutputReg[1] = CmdByte;
 	ret = write(I2CFile, APM_OutputReg, 2);
-	APM_st_msg.c1_status = (CmdByte & 0x01) ? 0 : 1;
+	pca9536->APM_st_msg.c1_status = (CmdByte & 0x01) ? 0 : 1;
 	
 	printf("Pin-1 state is : HIGH");
 	
