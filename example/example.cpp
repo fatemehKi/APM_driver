de0e2f3c3ffaf6d324655adf1773f44c641a8d2d
fatemehKi <40069260+fatemehKi@users.noreply.github.com>
2018-07-11 21:23:46 +0000

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -108,7 +108,7 @@ int main()
   	char txBuffer[32];  // transmit buffer
 	PCA9536 *pca9536 = new PCA9536() ;
 	//PCA9536 :: ch_status APM_st_msg;
-	APM_st_msg.c1_status = (CmdByte & 0x01) ? 0 : 1;
+	//APM_st_msg.c1_status = (CmdByte & 0x01) ? 0 : 1;
 		
 	char fileNameBuffer[32];
 	//sprintf(fileNameBuffer,"/dev/i2c-%d", kI2CBus);
